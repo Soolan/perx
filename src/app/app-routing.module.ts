a71e2e1e7e8a0dd5edce9a6974f25d4e22d808de
Soolan <info@soolan.com>
2019-11-28 13:23:18 +0800

    add missing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TableComponent} from './component/table/table.component';
+import {MyDirectivesComponent} from './component/my-directives/my-directives.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'table', component: TableComponent},
+  {path: 'directives', component: MyDirectivesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './component/navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { TableComponent } from './component/table/table.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { WhereAreMyPerxDirective } from './directive/where-are-my-perx.directive';
-import { PerxDirective } from './directive/perx.directive';
 import { MyDirectivesComponent } from './component/my-directives/my-directives.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import { TableComponent } from './component/table/table.component';
+import { PerxDirective } from './directive/perx.directive';
+import { WhereAreMyPerxDirective } from './directive/where-are-my-perx.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    MyDirectivesComponent,
     TableComponent,
-    WhereAreMyPerxDirective,
     PerxDirective,
-    MyDirectivesComponent
+    WhereAreMyPerxDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
@@ -40,7 +42,9 @@ import { MyDirectivesComponent } from './component/my-directives/my-directives.c
     MatListModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
